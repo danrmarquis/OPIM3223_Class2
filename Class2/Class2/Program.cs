a42ce0086a43d2ad554b2e83b82b03b229aca756
Dan Marquis <31213075+danrmarquis@users.noreply.github.com>
2019-09-09 16:52:24 -0400

    Add files via upload

---
+++ Class2/Class2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Class2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            // STRINGS
+
+            // The most simple example of C#: Hello World!
+            Console.WriteLine("Hello World!");
+
+            // strings and variables
+
+            // string interpolation
+
+            // methods and parameters
+
+            // searching strings
+
+
+
+            // LOGIC
+
+            // if statements
+
+            // else 
+
+            // operators
+
+            // loops (while and for)
+
+
+            // NUMBERS
+
+            // basic math
+
+            // order of operations
+
+            // integers and min/max
+
+            // doubles
+
+            // C# built in values
+
+
+        }
+    }
+}
